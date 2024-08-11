9270a9f06a6fb1d2c5dc5117ddef162f10fa7ca3
Omar mohamed <omarabdou2002@yahoo.com>
2024-08-11 21:16:30 +0300

    Task 3

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,11 +1,9 @@
-// import { useState } from 'react'
-// import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
-import './App.css'
+
 import WelcomeMessage from './components/WelcomeMessage'
 import Header from './components/Header'
 import MainContent from './components/MainContent'
 import Footer from './components/Footer'
+import UserProfile from './components/UserProfile'
 
 
 function App() {
@@ -15,6 +13,7 @@ function App() {
       <Header />
       <MainContent />
       <Footer />
+      <UserProfile name="Alice" age="25" bio="Loves hiking and photography"/>
     </>
   )
 }
