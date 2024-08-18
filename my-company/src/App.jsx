5e14a7d4c4d569837f363bd5034451dd23dc3905
Omar mohamed <omarabdou2002@yahoo.com>
2024-08-18 19:43:12 +0300

    2

--- my-company/src/App.jsx
+++ my-company/src/App.jsx
@@ -1,11 +1,10 @@
-import './App.css';
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 import Home from './components/Home';
 import Services from './components/Services';
 import Contact from './components/Contact';
 import About from './components/About';
 import Footer from './components/Footer';
-import Navbar from './components/Navbar';
+// import Navbar from './components/Navbar';
 
 
 function App() {
