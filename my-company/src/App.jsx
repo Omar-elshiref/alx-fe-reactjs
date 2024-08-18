f9afbd335842a89ed5ff9abaab731cf04a8cd142
Omar mohamed <omarabdou2002@yahoo.com>
2024-08-18 20:35:58 +0300

    add router

--- my-company/src/App.jsx
+++ my-company/src/App.jsx
@@ -1,22 +1,30 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom"
-import Home from './components/Home';
-import Services from './components/Services';
-import Contact from './components/Contact';
-import About from './components/About';
-import Footer from './components/Footer';
-// import Navbar from './components/Navbar';
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./components/Home";
+import Services from "./components/Services";
+import Contact from "./components/Contact";
+import About from "./components/About";
+import Footer from "./components/Footer";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
-    <div className="App">
-      <Home />
+    <div className='App'>
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/Services.jsx' element={<Services />} />
+          <Route path='/Contact.jsx' element={<Contact />} />
+          <Route path='/About.jsx' element={<About />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
+      {/* <Home /> */}
       {/* <Navbar /> */}
-      <Services />
+      {/* <Services />
       <Contact />
       <About />
-      <Footer />
-     
+      <Footer /> */}
     </div>
   );
 }
--- my-company/src/components/Navbar.jsx
+++ my-company/src/components/Navbar.jsx
@@ -1,15 +1,13 @@
-import React from 'react'
 import { Link } from 'react-router-dom'
 
 function Navbar() {
   return (
-    <header>
-        <nav>
-            <Link to="/">Home</Link>
-            <Link to="./Services.jsx">Services</Link>
-            <Link to="./Contact.jsx">Contact</Link>
-            <Link to="./About.jsx">About</Link>
-
+    <header style={{backgroundColor: "red", padding: "10px"}}>
+        <nav style={{color: "white", display: "flex", justifyContent: "center", gap: "60px"}}>
+            <Link style={{color: "white", textDecoration: "none", padding: "10px", backgroundColor: "blue", borderRadius: "10PX"}} to="/">Home</Link>
+            <Link style={{color: "white", textDecoration: "none", padding: "10px", backgroundColor: "blue", borderRadius: "10PX"}} to="./Services.jsx">Services</Link>
+            <Link style={{color: "white", textDecoration: "none", padding: "10px", backgroundColor: "blue", borderRadius: "10PX"}} to="./Contact.jsx">Contact</Link>
+            <Link style={{color: "white", textDecoration: "none", padding: "10px", backgroundColor: "blue", borderRadius: "10PX"}} to="./About.jsx">About</Link>
         </nav>
     </header>
   )
