ca89dda774e7aefd64c5c3870abed5ca0c883d16
Omar mohamed <omarabdou2002@yahoo.com>
2024-08-18 17:29:52 +0300

    useContext

---
+++ alx-react-app-props/src/components/Footer.jsx
@@ -0,0 +1,11 @@
+function Footer() {
+  return (
+    <>
+      <footer style={{textAlign:"center", backgroundColor: "green", padding: "10px", color: "white"}}>
+        <p>© 2023 City Lovers</p>
+      </footer>
+    </>
+  );
+}
+
+export default Footer;
---
+++ alx-react-app-props/src/components/MainContent.jsx
@@ -0,0 +1,11 @@
+function MainContent() {
+  return (
+    <>
+      <main style={{textAlign: "center", backgroundColor: "red", padding: "10px", color: "white"}}>
+        <p>I love to visit New York, Paris, and Tokyo.</p>
+      </main>
+    </>
+  );
+}
+
+export default MainContent;
