c27e58bafddac4055e4fec06066028d5e77c7fb8
Omar mohamed <omarabdou2002@yahoo.com>
2024-08-25 21:38:50 +0300

    Task 2

--- recipe-sharing-app/src/components/RecipeList.jsx
+++ recipe-sharing-app/src/components/RecipeList.jsx
@@ -1,12 +1,16 @@
-import { useRecipeStore } from "./recipeStore";
+import { Link } from 'react-router-dom';
+import { useRecipeStore } from './recipeStore';
 
 const RecipeList = () => {
   const recipes = useRecipeStore((state) => state.recipes);
+
   return (
     <div>
       {recipes.map((recipe) => (
         <div key={recipe.id}>
-          <h3>{recipe.title}</h3>
+          <h3>
+            <Link to={`/recipe/${recipe.id}`}>{recipe.title}</Link>
+          </h3>
           <p>{recipe.description}</p>
         </div>
       ))}
