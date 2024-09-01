8dafa1c055eb8db00b6fe439b8d2c820cd3e987d
Omar mohamed <omarabdou2002@yahoo.com>
2024-09-01 08:12:47 +0300

    Task 0

--- form-handling-react/src/components/RegistrationForm.jsx
+++ form-handling-react/src/components/RegistrationForm.jsx
@@ -4,22 +4,11 @@ const RegistrationForm = () => {
   const [username, setUsername] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-//   const [errors, setErrors] = useState({});
 
-//   const validate = () => {
-//     let errors = {};
-//     if (!username) errors.username = "Username is required";
-//     if (!email) errors.email = "Email is required";
-//     if (!password) errors.password = "Password is required";
-//     return errors;
-//   };
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    // const validationErrors = validate();
-    // if (Object.keys(validationErrors).length > 0) {
-    //   setErrors(validationErrors);
-    // } else {
+  
       console.log({ username, email, password });
     // }
   };
@@ -37,7 +26,6 @@ const RegistrationForm = () => {
             required
             onChange={(e) => setUsername(e.target.value)}
           />
-          {/* {errors.username && <p>{errors.username}</p>} */}
         </div>
 
         <div>
@@ -50,7 +38,6 @@ const RegistrationForm = () => {
             required
             onChange={(e) => setEmail(e.target.value)}
           />
-          {/* {errors.email && <p>{errors.email}</p>} */}
         </div>
 
         <div>
@@ -63,7 +50,6 @@ const RegistrationForm = () => {
             required
             onChange={(e) => setPassword(e.target.value)}
           />
-          {/* {errors.password && <p>{errors.password}</p>} */}
         </div>
         <button type='submit'>Submit</button>
       </form>
