b319f113d1fe2c1adb8cc1a9fd48dd1451478179
Omar mohamed <omarabdou2002@yahoo.com>
2024-09-01 08:10:44 +0300

    Task 0

--- form-handling-react/src/components/RegistrationForm.jsx
+++ form-handling-react/src/components/RegistrationForm.jsx
@@ -1,55 +1,86 @@
 import { useState } from "react";
 
 const RegistrationForm = () => {
-    const [value, setValue] = useState({ username: '', email: '',  password: ''});
-  const handleValues = (e) => {
-    const { name, value } = e.target;
-    setValue(prevState => ({ ...prevState, [name]: value }));
-  };
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+//   const [errors, setErrors] = useState({});
+
+//   const validate = () => {
+//     let errors = {};
+//     if (!username) errors.username = "Username is required";
+//     if (!email) errors.email = "Email is required";
+//     if (!password) errors.password = "Password is required";
+//     return errors;
+//   };
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log(value);
-};
+    // const validationErrors = validate();
+    // if (Object.keys(validationErrors).length > 0) {
+    //   setErrors(validationErrors);
+    // } else {
+      console.log({ username, email, password });
+    // }
+  };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <input
-        type='text'
-        name='username'
-        id='username'
-        value={value.username}
-        onChange={handleValues}
-      />
-      <input type='email' name='email' id='email' value={value.email} onChange={handleValues} />
-      <input
-        type='password'
-        name='password'
-        id='password'
-        value={value.password}
-        onChange={handleValues}
-      />
-      <button type="submit"></button>
-    </form>
+    <div>
+      <form onSubmit={handleSubmit} style={{display: "flex", flexDirection: "column", gap: "3rem"}}>
+        <div>
+          <label htmlFor='username' style={{marginRight: "10px"}}>Username</label>
+          <input
+            type='text'
+            id='username'
+            name='username'
+            value={username}
+            required
+            onChange={(e) => setUsername(e.target.value)}
+          />
+          {/* {errors.username && <p>{errors.username}</p>} */}
+        </div>
+
+        <div>
+          <label htmlFor='email' style={{marginRight: "10px"}}>Email</label>
+          <input
+            type='email'
+            id='email'
+            name='email'
+            value={email}
+            required
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          {/* {errors.email && <p>{errors.email}</p>} */}
+        </div>
+
+        <div>
+          <label htmlFor='password' style={{marginRight: "10px"}}>Password</label>
+          <input
+            type='password'
+            id='password'
+            name='password'
+            value={password}
+            required
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          {/* {errors.password && <p>{errors.password}</p>} */}
+        </div>
+        <button type='submit'>Submit</button>
+      </form>
+    </div>
   );
 };
 
 export default RegistrationForm;
 
-
-
-
 // import { Formik, Form, Field, ErrorMessage } from 'formik';
 // import * as Yup from 'yup';
-
 
 // const validationSchema = Yup.object({
 //     name: Yup.string().required('Name is required'),
 //     email: Yup.string().email('Invalid email').required('Email is required'),
 //     password: Yup.string().required('password is required')
 // });
-
-
 
 // const RegistrationForm = () => (
 //     <Formik
