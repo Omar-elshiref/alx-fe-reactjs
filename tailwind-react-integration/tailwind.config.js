af16493778e356556abd63032aaaee1ff188297c
Omar mohamed <omarabdou2002@yahoo.com>
2024-09-06 22:30:08 +0300

    Task 0

--- tailwind-react-integration/tailwind.config.js
+++ tailwind-react-integration/tailwind.config.js
@@ -1,12 +1,7 @@
-/** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
   darkMode: false,
-  theme: {
-    extend: {},
-  },
+  theme: { extend: {} },
+  variants: { extend: {} },
   plugins: [],
-}
+};
