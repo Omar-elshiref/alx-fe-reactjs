5fa78f747828009c6277dfc3188d099f561cf445
Omar mohamed <omarabdou2002@yahoo.com>
2024-09-13 00:11:16 +0300

    Task 3

--- recipe-sharing-platform/src/components/AddRecipeForm.jsx
+++ recipe-sharing-platform/src/components/AddRecipeForm.jsx
@@ -29,16 +29,16 @@ const AddRecipeForm = () => {
 
 
   return (
-    <div className="w-96 m-auto">
-      <form className="flex flex-col justify-center" onSubmit={handleSubmit}>
-        <label htmlFor='recipe-title'>recipe title</label>
-        <input type='text' name='recipe-title' id='recipe-title' onChange={(e) => setRecipe(e.target.value)} />
+    <div className="h-lvh flex justify-center items-center">
+      <form className="flex flex-col justify-center rounded-lg shadow-md border-2 border-orange-100 p-3 gap-3 w-96" onSubmit={handleSubmit}>
+        <label className="font-bold" htmlFor='recipe-title'>recipe title</label>
+        <input className="border-2 border-orange-100" type='text' name='recipe-title' id='recipe-title' onChange={(e) => setRecipe(e.target.value)} />
         {errors.recipe && <p>{errors.recipe}</p>}
-        <label htmlFor='ingredients'>ingredients</label>
-        <textarea type="text" name='ingredients' id='ingredients' onChange={(e) => setIngredients(e.target.value)}></textarea>
+        <label className="font-bold" htmlFor='ingredients'>ingredients</label>
+        <textarea className="border-2 border-orange-100" type="text" name='ingredients' id='ingredients' onChange={(e) => setIngredients(e.target.value)}></textarea>
         {errors.ingredients && <p>{errors.ingredients}</p>}
-        <label htmlFor='preparation'>preparation steps</label>
-        <textarea type="text" name='preparation' id='preparation' onChange={(e) => setPreparation(e.target.value)}></textarea>
+        <label className="font-bold" htmlFor='preparation'>preparation steps</label>
+        <textarea className="border-2 border-orange-100" type="text" name='preparation' id='preparation' onChange={(e) => setPreparation(e.target.value)}></textarea>
         {errors.preparations && <p>{errors.preparations}</p>}
         <button type="submit" className='text-white bg-indigo-700 mt-4 inline-block p-2 rounded-lg hover:bg-indigo-950'>Submit</button>
       </form>
