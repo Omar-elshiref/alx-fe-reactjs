f2f343adc9ea044d19b7577a6912e01fcd86a6df
Omar mohamed <omarabdou2002@yahoo.com>
2024-09-20 22:59:35 +0300

    Task 1

--- github-user-search/src/App.jsx
+++ github-user-search/src/App.jsx
@@ -1,29 +1,9 @@
-import { useState } from 'react';
 import Search from './components/Search';
-import UserDetails from './components/UserDetails';
-import { fetchUserData } from './services/githubService';
 
 const App = () => {
-  const [user, setUser] = useState(null);
-  const [error, setError] = useState('');
-
-  const handleSearch = async (username) => {
-    try {
-      setError('');
-      const userData = await fetchUserData(username);
-      setUser(userData);
-    } catch (err) {
-      setError(err.message);
-      setUser(null);
-    }
-  };
-
   return (
     <div>
-      <h1 className='text-center p-10 text-2xl'>GitHub User Search</h1>
-      <Search onSearch={handleSearch} />
-      {error && <p style={{ color: 'red' }}>{error}</p>}
-      <UserDetails user={user} />
+      <Search />
     </div>
   );
 };
--- github-user-search/src/components/Search.jsx
+++ github-user-search/src/components/Search.jsx
@@ -1,26 +1,68 @@
-import { useState } from 'react';
+import { useState } from "react";
+import { fetchUserData } from "../services/githubService";
 
-const Search = ({ onSearch }) => {
-  const [username, setUsername] = useState('');
+const Search = () => {
+  const [username, setUsername] = useState("");
+  const [user, setUser] = useState(null);
+  const [error, setError] = useState("");
+  const [loading, setLoading] = useState(false);
 
-  const handleSubmit = (e) => {
+  const handleSearch = async (e) => {
     e.preventDefault();
-    if (username.trim()) {
-      onSearch(username);
+    setLoading(true);
+    setError("");
+    setUser(null);
+    try {
+      const userData = await fetchUserData(username);
+      setUser(userData);
+    } catch {
+      setError("Looks like we cant find the user");
     }
+    setLoading(false);
   };
 
   return (
-    <form onSubmit={handleSubmit} className='flex justify-center'>
-      <input
-      className='border-2 border-indigo-600 p-1 rounded-lg w-72'
-        type="text"
-        value={username}
-        onChange={(e) => setUsername(e.target.value)}
-        placeholder="Enter GitHub username"
-      />
-      <button type="submit" className='bg-indigo-600 text-white p-3 ml-2 rounded-lg hover:bg-black'>Search</button>
-    </form>
+    <div>
+      <h1 className='text-center p-10 text-2xl'>GitHub User Search</h1>
+      <form onSubmit={handleSearch} className='flex justify-center'>
+        <input
+          className='border-2 border-indigo-600 p-1 rounded-lg w-72'
+          type='text'
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+          placeholder='Enter GitHub username'
+        />
+        <button
+          type='submit'
+          className='bg-indigo-600 text-white p-3 ml-2 rounded-lg hover:bg-black'>
+          Search
+        </button>
+      </form>
+
+      {loading && <p>Loading...</p>}
+      {error && <p className="text-red-500 text-2xl text-center p-7">{error}</p>}
+
+      {user && (
+        <div className='flex justify-center flex-col items-center p-14'>
+          <img
+            src={user.avatar_url}
+            alt={user.login}
+            width='100'
+            className='w-48 rounded-full'
+          />
+          <h3 className='text-4xl p-5'>{user.login}</h3>
+          <p className='p-3 text-xl'>Followers: {user.followers}</p>
+          <p className='p-3 text-xl'>Following: {user.following}</p>
+          <a
+            className='mt-2 bg-indigo-600 text-white p-3 ml-2 rounded-lg hover:bg-black'
+            href={user.html_url}
+            target='_blank'
+            rel='noopener noreferrer'>
+            View Profile on GitHub
+          </a>
+        </div>
+      )}
+    </div>
   );
 };
 
